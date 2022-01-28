5446ff38a0b40bd75945efa25d4ba49af91f73fc
Watson <98172889+XinwenWatson@users.noreply.github.com>
2022-01-28 09:48:41 -0500

    added comments01

--- src/student_w22_fri/Student.java
+++ src/student_w22_fri/Student.java
@@ -1,8 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+author: Xinwen Li
+Date: Jan 28, 2022
+*/
 package student_w22_fri;
 
 /**
